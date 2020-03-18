25bc65018483ac0971976ae00d7fcadbd15d98ea
Ace Eldeib <aleldeib@microsoft.com>
2020-03-17 19:20:01 -0700

    tweak: pull geneva info from environment

--- metrics/utils/utils.py
+++ metrics/utils/utils.py
@@ -4,8 +4,8 @@ import json
 def generate_payload(metric_name, metric_value):
     # JSON payload to be sent to Geneva
     payload = {
-        "Account": "AzureUpstreamInfra",
-        "Namespace": "AzureUpstreamInfra",
+        "Account": os.getenv("GENEVA_ACCCOUNT"),
+        "Namespace": os.getenv("GENEVA_NAMESPACE"),
         "Metric": metric_name,
         "Dims": {
             "JobName": os.getenv("AGENT_JOBNAME"),
